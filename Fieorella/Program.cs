ca379c716a71b9a3c1028aca6a48ef2666d61e7f
khankishiekhbpa203 <khankishiekh-bpa203@code.edu.az>
2025-12-16 18:13:16 +0400

    taskupdated1

--- Fieorella/Program.cs
+++ Fieorella/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 var app = builder.Build();
+
 app.UseStaticFiles();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
